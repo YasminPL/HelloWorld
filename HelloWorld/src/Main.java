330d482c0d9a0dc94ffeec957da3a1edf52d0ff0
Dylan Barbona <dylanbarbona@users.noreply.github.com>
2020-04-16 14:11:17 -0300

    Soluciono conflicto

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -2,8 +2,11 @@
 public class Main {
 
 	public static void main(String[] args) {
+<<<<<<< HEAD
+=======
 		System.out.println("Probando git con cbancala");
 
+>>>>>>> 72c48a38140858ca3a37130dce254da1081b8af4
 	}
 
 }
