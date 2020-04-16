73879c4449ef2e3639df9c304f9bd88cf70fb4ae
Facu <gonzalofacu@gmail.com>
2020-04-16 17:20:33 -0300

    Revert "Update Main.java"
    This reverts commit aaf57fbe60d5c41415c19f3c6971fee77ee83ae7, reversing
    changes made to 741bdbae32b98bdc5d251c6863d063749694a8df.

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -3,10 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-<<<<<<< HEAD
 		System.out.println("Probando git con cbancala, Yasmin, Dylan y Facu");
-=======
->>>>>>> 3e3db445376078fd6e9953f7b774a36fc1c8f1c3
 
 	}
 
